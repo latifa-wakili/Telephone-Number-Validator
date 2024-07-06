b70c2202d9d554987bfd34ec059a945c3cb5d9fd
latifa <latifawakili3@gmail.com>
2024-07-05 22:43:53 -0700

    checking for resolveing errores

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ document.getElementById('check-btn').addEventListener('click', () => {
   const resultsDiv = document.getElementById('results-div');
 
   if (userInput === '') {
-alert(" Please provide a phone number");
-    return;
+Alert('PleaSe provide a phone number');
+  return;
   }
 
   const regex = /^(1\s?)?(\(\d{3}\)|\d{3})[-\s]?\d{3}[-\s]?\d{4}$/;
