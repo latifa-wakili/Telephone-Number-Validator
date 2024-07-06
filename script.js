5bae15df67870ad2178a17d1bb4bf641c7d9dbee
latifa <latifawakili3@gmail.com>
2024-07-05 22:36:22 -0700

    added new style and picture

--- script.js
+++ script.js
@@ -1,42 +1,22 @@
-const input = document.getElementById('user-input');
-const checkBtn = document.getElementById('check-btn');
-const clearBtn = document.getElementById('clear-btn');
-const resultsDiv = document.getElementById('results-div');
-const regexPattern = /^(1\s|1)?(\(\d{3}\)|\d{3})(-|\s)?\d{3}(-|\s)?\d{4}$/;
+document.getElementById('check-btn').addEventListener('click', () => {
+  const userInput = document.getElementById('user-input').value;
+  const resultsDiv = document.getElementById('results-div');
 
+  if (userInput === '') {
+alert(" Please provide a phone number");
+    return;
+  }
 
-const hideResults = () => {
-   resultsDiv.style.display = 'none';
-}
-hideResults();
+  const regex = /^(1\s?)?(\(\d{3}\)|\d{3})[-\s]?\d{3}[-\s]?\d{4}$/;
 
-const alertEmpty = () => {
-    const inputVal = input.value;
-    if (inputVal === '') {
-        alert("Please provide a phone number");
-    } 
-}
-checkBtn.addEventListener('click', alertEmpty);
+  if (regex.test(userInput)) {
+    resultsDiv.innerText = `Valid US number: ${userInput}`;
+  } else {
+    resultsDiv.innerText = `Invalid US number: ${userInput}`;
+  }
+});
 
-
-const isNumValid = () => {
-    const inputVal = input.value.trim();
-    const matchResult = inputVal.match(regexPattern);
-    if (matchResult !== null) {
-        resultsDiv.style.display = 'flex';
-        resultsDiv.innerHTML = `<p id="inserted-p">Valid US number: ${inputVal}</p>`;
-    } else {
-        resultsDiv.style.display = 'flex';
-        resultsDiv.innerHTML = `<p id="inserted-p">Invalid US number: ${inputVal}</p>`;
-    }
-}
-
-checkBtn.addEventListener('click', isNumValid);
-
-const removeP = () => {
-    const insertedP = document.getElementById('inserted-p');
-    if (insertedP) {
-    resultsDiv.removeChild(insertedP); 
-    }
-}
-clearBtn.addEventListener('click', removeP);
+document.getElementById('clear-btn').addEventListener('click', () => {
+  const resultsDiv = document.getElementById('results-div');
+  resultsDiv.innerText = '';
+});
